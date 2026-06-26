661e717b5bb195a8d488e9f0c7fe8585467e9137
Rosdeb koch <rosdevkoch@gmail.com>
2026-06-26 21:23:29 +0600

    mainActivity search..

--- android/app/src/main/kotlin/com/pilach/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/pilach/app/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.pilach.app
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import com.pilach.app.NotificationPlugin
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        NotificationPlugin().onAttachedToEngine(flutterEngine.dartExecutor.binaryMessenger, this)
-    }
-}
+class MainActivity : FlutterActivity()
